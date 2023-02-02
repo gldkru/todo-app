fec4efd87d7b3d2f79c8ba499869d3e130a74cc7
Kirill Gladkikh <onest.kill@yandex.ru>
2023-02-02 06:30:40 +0000

    handle duplicate taks, handle empty task error

--- src/App.js
+++ src/App.js
@@ -7,18 +7,37 @@ import "./styles.css";
 export default function App() {
   const [taskList, setTaskList] = useState(["first", "second"]);
   const [completedTaskList, setCompletedTaskList] = useState(["second"]);
+  const [error, setError] = useState("");
 
   const handleCompleteTask = (task) => {
     if (completedTaskList.includes(task)) {
-      setCompletedTaskList([...completedTaskList.filter((t) => t !== task)]);
+      setCompletedTaskList(completedTaskList.filter((t) => t !== task));
     } else {
       setCompletedTaskList([...completedTaskList, task]);
     }
   };
 
+  const onSubmit = (task) => {
+    // сброс ошибки
+    setError("");
+
+    // пустая задача
+    if (!task) {
+      return;
+    }
+
+    if (taskList.includes(task)) {
+      setError("Такая задача уже есть");
+      return;
+    }
+
+    setTaskList([...taskList, task]);
+  };
+
   return (
     <div className="App">
-      <Form onSubmit={(task) => setTaskList([...taskList, task])} />
+      <Form onSubmit={onSubmit} />
+      {error && <div>{error} <button onClick={() => setError('')}>x</button></div>}
       <div>{JSON.stringify(taskList)}</div>
       <div>{JSON.stringify(completedTaskList)}</div>
       <div>
